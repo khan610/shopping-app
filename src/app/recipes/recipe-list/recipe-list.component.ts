5cefef4497d2ce44b8d54e99280b944ea01b1b4c
Andrea Trajkovic <trajkovicandrea@gmail.com>
2024-02-07 16:07:28 +0100

    Update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://panlasangpinoy.com/wp-content/uploads/2023/10/tofu-and-broccoli-stir-fry-750x750.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://panlasangpinoy.com/wp-content/uploads/2023/10/tofu-and-broccoli-stir-fry-750x750.jpg'
+    ),
+  ];
 }
